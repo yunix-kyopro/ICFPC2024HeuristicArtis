fc6df29185932c3812c3bbde9fcec3db9a973baa
jirotech <shuu09142000@gmail.com>
2024-07-01 21:16:10 +0900

    update lambdaman-20

--- lambdaman/out_js/20.mjs
+++ lambdaman/out_js/20.mjs
@@ -29,76 +29,58 @@ const Ycomb = lambda((f) =>
   )
 );
 
-const concats = (...args) => {
-  let ret = concat(args[0], args[1]);
-  for (let i = 2; i < args.length; i++) {
-    ret = concat(ret, args[i]);
-  }
-  return ret;
-};
-
-const repeat = lambda((g) =>
-  lambda((m) =>
-    lambda((c) =>
-      if_(
-        eq(m, integer(0)),
-        string(""),
-        concat(c, app(app(g, sub(m, integer(1))), c))
-      )
-    )
-  )
+const ldru = lambda((idx) =>
+  head(integer(1), tail(mod(idx, integer(4)), string("LDRU")))
 );
 
-const urdl = lambda((idx) =>
-  head(integer(1), tail(mod(idx, integer(4)), string("URDL")))
-);
-
-const solve = app(
-  lambda((lrud) =>
-    lambda((rep) =>
-      lambda((self) =>
-        lambda((n) =>
-          lambda((idx) =>
+const saiki = lambda((self) =>
+  lambda((ldru) =>
+    lambda((n) =>
+      lambda((firstDirIdx) =>
+        lambda((isHalf) =>
+          lambda((t) =>
             if_(
-              lt(n, integer(1)),
-              string(""),
-              concats(
-                app(app(rep, div(n, integer(2))), app(lrud, idx)),
-                app(app(self, div(n, integer(4))), add(idx, integer(3))),
-                app(app(self, div(n, integer(4))), add(idx, integer(1))),
-                app(app(rep, div(n, integer(2))), app(lrud, idx)),
-                app(app(self, div(n, integer(2))), add(idx, integer(3))),
-                app(app(self, div(n, integer(2))), add(idx, integer(0))),
-                app(app(self, div(n, integer(2))), add(idx, integer(1))),
-                app(app(rep, n), app(lrud, add(idx, integer(2))))
+              lt(mul(n, isHalf), integer(2)),
+              app(ldru, firstDirIdx),
+              if_(
+                eq(t, mul(n, integer(12))),
+                string(""),
+                concat(
+                  if_(
+                    lt(t, mul(n, integer(4))),
+                    app(ldru, firstDirIdx),
+                    if_(
+                      lt(t, mul(n, integer(8))),
+                      app(app(app(app(app(self, ldru),
+                        div(n, integer(2))),
+                        add(add(add(firstDirIdx, div(t, n)), mul(isHalf, integer(2))), integer(1))),
+                        add(div(t, add(mul(integer(7), n), div(n, integer(128)))), integer(1))),
+                        integer(0)),
+                      app(ldru, add(firstDirIdx, integer(2)))
+                    )
+                  ),
+                  app(app(app(app(app(self, ldru),
+                    n),
+                    firstDirIdx),
+                    isHalf),
+                    add(
+                      t,
+                      if_(
+                        gt(mod(div(t, n), integer(8)), integer(3)),
+                        mul(isHalf, n),
+                        integer(4)
+                      )
+                    )
+                  )
+                )
               )
             )
           )
         )
       )
     )
-  ),
-  urdl
-);
-
-const final = app(
-  lambda((ycomb) => app(ycomb, app(solve, app(ycomb, repeat)))),
-  Ycomb
-);
-
-const solveLambdaman20 = concat(
-  string("solve lambdaman20 "),
-  app(
-    lambda((f) =>
-      concats(
-        app(app(f, integer(64)), integer(0)),
-        app(app(f, integer(64)), integer(1)),
-        app(app(f, integer(64)), integer(2)),
-        app(app(f, integer(64)), integer(3))
-      )
-    ),
-    final
   )
-);
+)
 
+const solveLambdaman20 = concat(string("solve lambdaman20 "), app(app(app(app(app(app(Ycomb, saiki), ldru), integer(128)), integer(1)), integer(1)), integer(128 * 4)));
 console.log(evaluate(solveLambdaman20));
